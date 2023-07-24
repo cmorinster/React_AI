c356a976a8fb485661c05e353cf3f121b0351b74
Christopher Morin <cmorinster@gmail.com>
2023-07-23 22:29:07 -0400

    some changes to about story(prooreading). icons now clickable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,24 +20,34 @@ export default function Navbar({ loggedIn, logUserOut }) {
                 
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav mr-auto justify-content-end">
+                    <Link to="/">
                     <img src={Dragon} alt="Logo" className ="dragonLogo" />
-                        
+                    </Link>  
                         <Link className="nav-link" to="/">Home</Link>
-                        <img src={Lion} alt="Logo" className ="lionLogo" />
+                        
+                        
                         
                     
                         {loggedIn ? (
                             <>
+                            <Link to="/create">
+                            <img src={Lion} alt="Logo" className ="lionLogo" />
+                            </Link>
                             <Link className="nav-link" to="/create">Create A Character</Link>
-        
+                            <Link to="/" onClick={() => logUserOut()}>
                             <img src={Knight} alt="Logo" className ="knightLogo" />
+                            </Link>
                             <Link className="nav-link" to="/" onClick={() => logUserOut()}>Log Out</Link>
                             </>
                         ) : (
                             <>
+                            <Link to="/register">
+                            <img src={Lion} alt="Logo" className ="lionLogo" />
+                            </Link>
                             <Link className="nav-link" to="/register">Register</Link>
-                            
+                            <Link to="/login">
                             <img src={Knight} alt="Logo" className ="knightLogo" />
+                            </Link>
                             <Link className="nav-link" to="/login">Log In</Link>
                             </>
                         )}
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
     <br></br>
     <h6 className="card-subtitle mb-2 text-muted">About the App:</h6>
     <br></br>
-    <p className="card-text">Welcome to the RoboArt Arena where characters that you create using AI art images are pitted against against the reigning champion in a fierce battle. Then when the battles over chatGPT will narrate the great tale.  And if you win your character will be featured on the home page!  To begin register and login to start creating characters!  Good Luck!</p>
+    <p className="card-text">Welcome to the RoboArt Arena where characters that you create using AI art images are pitted against the reigning champion in a fierce battle. Then when the battle's over chatGPT will narrate the great tale.  And if you win your character will be featured on the home page!  To begin register and login to start creating characters!  Good Luck!</p>
         <Link className='card-link' to={`/`}>Home</Link>
         <Link className='card-link' to={`/register`}>Register</Link>
         <Link className='card-link' to={`/login`}>Login</Link>
