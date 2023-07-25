d66e70efde504c51a7b51035a8eb98aba72185e0
Christopher Morin <cmorinster@gmail.com>
2023-07-25 16:48:59 -0400

    added created by

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -10,6 +10,7 @@ export default function About() {
     <h6 className="card-subtitle mb-2 text-muted">About the App:</h6>
     <br></br>
     <p className="card-text">Welcome to the RoboArt Arena where characters that you create using AI art images are pitted against the reigning champion in a fierce battle. Then when the battle's over chatGPT will narrate the great tale.  And if you win your character will be featured on the home page!  To begin register and login to start creating characters!  Good Luck!</p>
+    <p className="card-text">Created by Christopher Morin</p>
         <Link className='card-link' to={`/`}>Home</Link>
         <Link className='card-link' to={`/register`}>Register</Link>
         <Link className='card-link' to={`/login`}>Login</Link>
